f2124f5295e213da17632bf1c3c257b6006f505b
moezabdelkef <moezbadelkefi17@gmail.com>
2023-09-06 16:49:17 +0100

    Lexical scoping and welcome message

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,9 @@
+function welcome(firstName, lastName) {
+    var fullName = firstName + ' ' + lastName
+
+    function displayFullName() {
+        alert("welcome " + fullName + "!");
+    }
+
+    displayFullName()
+}
